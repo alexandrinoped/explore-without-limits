1566f289c9fcbbfec78b74d1a26fb02f38317482
Pedro <pedro@dealersites.com.br>
2024-03-22 16:30:12 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+/*const iconItem = document.querySelectorAll('.head a')
+
+iconItem.forEach(item => {
+  item.addEventListener('click', scrollToIdOnClick)
+})
+
+function scrollToIdOnClick(event) {
+  event.preventDefault();
+  const element = event.target
+  const id = element.getAttribute('anchor')
+  const to = document.querySelector(id).offsettop
+
+  window.scroll({
+    top: to,
+    behavior: "smooth",
+  })
+}*/
